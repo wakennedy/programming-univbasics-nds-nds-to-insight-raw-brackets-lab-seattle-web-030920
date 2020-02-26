bd5b2a4ad56c36780839a77888ea00c6a49d3d21
William Kennedy <alexkenned@gmail.com>
2020-02-26 03:20:20 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-def directors_totals(nds)
+#def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
@@ -18,5 +18,22 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+ # nil
+#end
+
+def directors_totals(nds)
+list = {}
+directors_index = 0 
+  while directors_index < nds.length do 
+    title_index = 0 
+    grand_total = 0 
+    while title_index < nds[directors_index][:movies].length do
+      grand_total += nds[directors_index][:movies][title_index][:worldwide_gross]
+      title_index+=1
+    end
+    list["#{nds[directors_index][:name]}"] = grand_total
+    directors_index +=1 
+ end
+ pp list
 end
+
